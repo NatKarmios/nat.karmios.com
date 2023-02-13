18fa9b5df4d6495bc87bdc895ecf827002e11979
Nat Karmios <nat@karmios.com>
2023-02-13 06:41:19 +0000

    Attempt to fix mobile scaling

--- src/routes/layout.tsx
+++ src/routes/layout.tsx
@@ -1,16 +1,37 @@
-import { component$, Slot } from '@builder.io/qwik';
+import { component$, Slot, useClientEffect$ } from '@builder.io/qwik';
 
 import Header from '../components/header/header';
 import Footer from '../components/footer/footer';
 
+let pendingUpdate = false;
+
+export const updateScale = () => {
+  if (pendingUpdate) return;
+  pendingUpdate = true;
+  
+  requestAnimationFrame(() => {
+    pendingUpdate = false;
+    const root = document.querySelector(':root') as HTMLElement | null
+    if (root === null) return;
+    root.style.setProperty('--vw', `${window.innerWidth * 0.01}px`);
+    root.style.setProperty('--vh', `${window.innerHeight * 0.01}px`);
+  });
+};
+
+export const setupUpdateScale = () => {
+  updateScale();
+  window.onresize = updateScale;
+}
+
 export default component$(() => {
+  useClientEffect$(setupUpdateScale);
   return (
-    <>
+    <div class="body-inner">
       <div class="bg-cover" />
       <div class="bg-header-cover" />
       <Header />
       <Slot />
       <Footer />
-    </>
+    </div>
   );
 });
