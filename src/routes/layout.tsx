be62d8f48e811ea44a44425738e92d2a8c56e0b2
Nat Karmios <nat@karmios.com>
2023-02-13 13:05:25 +0000

    Attempt to fix iPhone masks

--- src/routes/layout.tsx
+++ src/routes/layout.tsx
@@ -13,8 +13,8 @@ export const updateScale = () => {
     pendingUpdate = false;
     const root = document.querySelector(':root') as HTMLElement | null
     if (root === null) return;
-    root.style.setProperty('--vw', `${window.innerWidth * 0.01}px`);
-    root.style.setProperty('--vh', `${window.innerHeight * 0.01}px`);
+    root.style.setProperty('--vw', `${window.visualViewport!.width * 0.01}px`);
+    root.style.setProperty('--vh', `${window.visualViewport!.height * 0.01}px`);
   });
 };
 
