40dc4671219b77cf73f1b1d851274c3f531ab7df
Nat Karmios <nat@karmios.com>
2023-02-15 11:33:47 +0000

    Tweak core skills

--- src/components/index/core-skills.tsx
+++ src/components/index/core-skills.tsx
@@ -33,10 +33,10 @@ export default component$(() => {
           My undergraduate studies involved specialist courses on Hoare Logic &mdash; including big- and small-step semantics &mdash; and Separation Logic for scalable program verification. My work frequently calls upon these principles, both in theoretical and practical contexts.
         </Skill>
         <Skill title="OCaml proficiency" img="ocaml">
-          The history of formal methods is entwined with that of OCaml. I've grown a deep appreciation for the language while working in this field, arming me with a natural understanding of advanced techniques, including the monadic programming style, and metaprogramming with modules and functors.
+          The history of formal methods is entwined with that of OCaml. I've grown a deep appreciation for the language while working in this field, arming me with a natural understanding of advanced techniques, including the monadic programming style, and abstracting via modules and functors.
         </Skill>
         <Skill title="Polyglot" img="langs">
-          As a self-taught hobbyist, turned software engineer, turned research engineer, my experience ranges over languages at all levels &mdash; from C and Rust to Haskell and OCaml; from Java and Kotlin to Python and JavaScript. A prime example is how my symbolic debugger, introduced a second language (TypeScript) Gillian's codebase.
+          As a self-taught hobbyist, turned software engineer, turned research engineer, my experience ranges over many language paradigms &mdash; from C and Rust to Haskell and OCaml; from Java and Kotlin to Python and JavaScript. A prime example is how my symbolic debugger, introduced a second language (TypeScript) Gillian's codebase.
         </Skill>
         <Skill title="Full stack" img="stack">
           In my numerous engineering roles, I've worked on applications at all levels &mdash; frontend, backend, and databases &mdash; from development to deployment, and maintaining servers thereafter. I'm well-versed in collaboration via Git and GitHub, and I'm comfortable in various development environments, whether it be Windows, Linux, WSL or Docker.
