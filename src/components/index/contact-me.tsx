7200619b9a353fa7def6abc39abe631891ce353f
Nat Karmios <nat@karmios.com>
2023-05-03 11:38:58 +0100

    Update Imperial email

--- src/components/index/contact-me.tsx
+++ src/components/index/contact-me.tsx
@@ -9,7 +9,7 @@ export default () => (
     </p>
     <ul>
       <li>My personal e-mail, <i class="email">nat [at] karmios [dot] com</i> .</li>
-      <li>...or failing that, my Imperial College email, <i class="email">nk4118 [at] ic [dot] ac [dot] uk</i> .</li>
+      <li>...or failing that, my Imperial College email, <i class="email">n.karmios [at] ic [dot] ac [dot] uk</i> .</li>
     </ul>
     <p>
       You're more than welcome to reach out via <A href={urls.linkedin}>LinkedIn</A>, however I check it <i>exceedingly</i> rarely.
